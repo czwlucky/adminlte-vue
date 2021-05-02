c1311f50e1003cb4e26758ed2430f76b1411de8b
check-words <czwlucky@qq.com>
2021-05-02 21:09:24 +0800

    全屏退出监听，自动改变样式

--- js/FullScreen.js
+++ js/FullScreen.js
@@ -1,47 +1,55 @@
-
 // TODO: 通过ESC键退出全屏时，图标不能触发改变
 var FullScreent = {
-		data() {
-			return {
-				minimized: true,
-				maximized: false,
+	data() {
+		return {
+			maximized: false
+		}
+	},
+	methods: {
+		toggle() {
+			if (document.fullscreenElement ||
+				document.mozFullScreenElement ||
+				document.webkitFullscreenElement ||
+				document.msFullscreenElement) {
+				this.windowed()
+			} else {
+				this.fullscreen()
 			}
 		},
-		methods: {
-			toggle() {
-			  if (document.fullscreenElement ||
-			    document.mozFullScreenElement ||
-			    document.webkitFullscreenElement ||
-			    document.msFullscreenElement) {
-			    this.windowed()
-			  } else {
-			    this.fullscreen()
-			  }
-			},
-			fullscreen() {
-			  if (document.documentElement.requestFullscreen) {
-			    document.documentElement.requestFullscreen()
-			  } else if (document.documentElement.webkitRequestFullscreen) {
-			    document.documentElement.webkitRequestFullscreen()
-			  } else if (document.documentElement.msRequestFullscreen) {
-			    document.documentElement.msRequestFullscreen()
-			  }
-			
-			  this.maximized = true;
-			  this.minimized = false;
-			},
-			windowed() {
-			  if (document.exitFullscreen) {
-			    document.exitFullscreen()
-			  } else if (document.webkitExitFullscreen) {
-			    document.webkitExitFullscreen()
-			  } else if (document.msExitFullscreen) {
-			    document.msExitFullscreen()
-			  }
-			
-			  this.maximized = false;
-			  this.minimized = true;
+		fullscreen() {
+			if (document.documentElement.requestFullscreen) {
+				document.documentElement.requestFullscreen()
+			} else if (document.documentElement.webkitRequestFullscreen) {
+				document.documentElement.webkitRequestFullscreen()
+			} else if (document.documentElement.msRequestFullscreen) {
+				document.documentElement.msRequestFullscreen()
 			}
+
+			//this.maximized = true;
 		},
-		template: `<a class="nav-link" @click.prevent="toggle" data-widget="fullscreen" href="#" role="button"><i :class="{'fa-expand-arrows-alt': minimized,'fa-compress-arrows-alt': maximized}" class="fas"></i></a>`
-	}
+		windowed() {
+			if (document.exitFullscreen) {
+				document.exitFullscreen()
+			} else if (document.webkitExitFullscreen) {
+				document.webkitExitFullscreen()
+			} else if (document.msExitFullscreen) {
+				document.msExitFullscreen()
+			}
+
+			//this.maximized = false;
+		}
+	},
+	mounted() {
+		var that = this;
+		window.addEventListener("resize", function() {
+			//console.log("status", document.fullscreenElement)
+			that.maximized = !!(document.fullscreenElement ||
+				document.mozFullScreenElement ||
+				document.webkitFullscreenElement ||
+				document.msFullscreenElement);
+			//console.log(that.maximized)
+		})
+		
+	},
+	template: `<a class="nav-link" @click.prevent="toggle" data-widget="fullscreen" href="#" role="button"><i :class="{'fa-expand-arrows-alt': !maximized,'fa-compress-arrows-alt': maximized}" class="fas"></i></a>`
+}
