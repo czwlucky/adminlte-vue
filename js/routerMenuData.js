fee089f05b7b0bbd092dad7ac39463004cf348b0
check-words <czwlucky@qq.com>
2021-05-26 15:26:40 +0800

    路由的使用

---
+++ js/routerMenuData.js
@@ -0,0 +1,23 @@
+var menus = [{
+	title: '导航菜单',
+	type: 'label'
+}, {
+	id: 'home',
+	title: 'Home',
+	url: '/',
+	icon: 'far fa-circle text-danger',
+	type: 'menu'
+}, {
+	id: 'b',
+	title: 'About',
+	url: '/about',
+	icon: 'far fa-circle text-warning',
+	type: 'menu'
+}, {
+	id: 'c',
+	title: 'Baidu',
+	url: 'http://www.baidu.com',
+	target: '_blank',
+	icon: 'far fa-circle text-info',
+	type: 'menu'
+}]
