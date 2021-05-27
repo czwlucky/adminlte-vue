cbcb719b8ad676af00568758e1412f860c28b66e
check-words <czwlucky@qq.com>
2021-05-27 13:21:11 +0800

    路由守卫实现菜单自动选中

--- js/routerMenuData.js
+++ js/routerMenuData.js
@@ -20,4 +20,37 @@ var menus = [{
 	target: '_blank',
 	icon: 'far fa-circle text-info',
 	type: 'menu'
+},{
+	id: 1,
+	title: '父菜单1',
+	icon: 'far fa-user',
+	children: [
+		{
+			id: 2,
+			parentId:1,
+			title: '子菜单1',
+			url: '/menu1'
+		},{
+			id: 3,
+			parentId:1,
+			title: '子菜单2',
+			url: '/menu2'
+		}
+	]
+},{
+	id: 4,
+	title: '父菜单2',
+	children: [
+		{
+			id: 5,
+			parentId:4,
+			title: '子菜单3',
+			url: '/menu3'
+		},{
+			id: 6,
+			parentId:4,
+			title: '子菜单4',
+			url: '/menu4'
+		}
+	]
 }]
