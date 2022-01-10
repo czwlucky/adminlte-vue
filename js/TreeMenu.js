f7a919adabd2cb1f545cf812e9ed8c5a93b4c89d
check-words <czwlucky@qq.com>
2022-01-10 22:15:44 +0800

    侧边栏菜单组件化

--- js/TreeMenu.js
+++ js/TreeMenu.js
@@ -1,26 +1,90 @@
+/**
+ * 该组件可展示无限级菜单
+ * menu.title > menu.label > menu.name，优先展示名称顺序
+ * menu.type，如果type=='label'，只做为标签进行显示，其它值视为菜单
+ * menu对象的isNew属性，可展示NEW标签
+ * menu对象的messageCount属性，可展示数字标签
+ * {
+        title:String, label:String, name:String,
+        icon:String,
+        url:String, path:String,
+        type:String,
+	      target:String,
+        id:String|Number,
+        parentId:String|Number,
+        isNew: Boolean,
+        messageCount: Number
+  }
+ */
 const MenuItem = {
   name: "LteTreeMenuItem",
   props: {
-      menu: {
-        title:String,
-        label:String,
-        name:String,
-        icon:String
-      },
-      opened: Boolean
+      menu: Object
   },
   setup(props, context) {
-    
+    const selectMenu = Vue.inject("selectMenu");
+    const menuItemRoot = Vue.ref(null);
+    const subMenus = Vue.ref(null);
+
+    const clickHandler = event => {
+      let menu = props.menu;
+      // 如果是外链，不改变菜单选中状态，直接返回，触发默认行为
+      if (menu.url && menu.url.indexOf("http") === 0) {
+        return;
+      }
+
+      event.preventDefault();
+
+      // 没有子菜单，直接触发菜单回调
+      if (!menu.children || menu.children.length == 0) {
+        selectMenu(menu);
+        return;
+      }
+
+      // 展开或关闭子菜单
+      let subMenuEl = subMenus.value;
+      if (!menu._opened_) {
+        // 必须先设置该样式，才可以以动画形式展开，否则动画脚本将得不到子菜单的实际高度，无法展开动画
+        menuItemRoot.value.classList.add("menu-open");
+        slideDown(subMenuEl, {
+          after: anmiationDone,
+          speed: 300 /*为了演示动画速度*/
+        });
+      } else {
+        //console.log("selectMenu close", subMenuEl)
+        slideUp(subMenuEl, {
+          after: anmiationDone,
+          speed: 300
+        })
+      }
+      // 动画结束之后，触发菜单回调
+      function anmiationDone() {
+        subMenuEl.style.overflow = "";
+        subMenuEl.style.height = "";
+        selectMenu(menu);
+      }
+    }
+
+    return {
+      menuItemRoot,
+      subMenus,
+      clickHandler
+    }
   },
   template: `
-    <li class="nav-item" :class="{
-      'menu-open': menu.opened,
+    <li ref="menuItemRoot" :class="{
       'nav-item': menu.type != 'label',
-      'nav-header': menu.type == 'label'
+      'nav-header': menu.type == 'label',
+      'menu-open': menu._opened_ && menu.children != null && menu.children.length > 0
        }">
       <template v-if="menu.type == 'label'">{{menu.title}}</template>
-      <a href="#" class="nav-link" :class="menu.isActive ? 'active':''" v-if="menu.type!='label'">
-        <i class="nav-icon" :class="menu.icon"></i>
+      <a class="nav-link"
+        :href="menu.url||menu.path||'#'"
+        :target="menu.target"
+        :class="{'active':menu._active_ }"
+        v-if="menu.type!='label'"
+        @click="clickHandler">
+        <i class="nav-icon" :class="menu.icon || 'far fa-circle'"></i>
         <p>
         {{menu.title || menu.label || menu.name}}
         <i class="right fas fa-angle-left" v-if="menu.children != null && menu.children.length > 0"></i>
@@ -28,7 +92,7 @@ const MenuItem = {
 				<span class="badge badge-info right" v-if="menu.messageCount > 0">{{menu.messageCount}}</span>
         </p>
       </a>
-      <ul class="nav nav-treeview" v-if="menu.type!='label' && menu.children != null && menu.children.length > 0">
+      <ul class="nav nav-treeview" ref="subMenus" v-if="menu.type!='label' && menu.children != null && menu.children.length > 0">
         <lte-tree-menu-item v-for="child of menu.children" :menu="child"></lte-tree-menu-item>
       </ul>
     </li>
@@ -40,18 +104,55 @@ const TreeMenu = {
     name: "LteTreeMenu",
     props: {
         menus: Array,
+        defaultOpenIndex: {
+          type: Number,
+          default: 0
+        }
     },
-    setup() {
+    setup(props, context) {
+      // 包装成响应式对象，更新时，子组件才会发生变动
+      const list = Vue.reactive(props.menus);
+
       const menuStyle = Vue.inject('menuStyle');
       console.log("inject menuStyle", menuStyle)
 
+      const selectMenu = menu=> {
+        menu._opened_ = !menu._opened_;
+        // 当前菜单如果是选中状态或有子菜单，不做处理
+        // 否则重新判断选中状态
+        if (menu._active_ || (menu.children && menu.children.length)) {
+          return
+        }
+        
+        list.forEach( item=> {
+          item._active_ = isActive(item);
+        })
+        function isActive(item) {
+          if (item.children && item.children.length) {
+            let ret = false;
+            for (const child of item.children) {
+              // 只要子孙有一个是被选中的，当前节点就是选中的
+              child._active_ = isActive(child);
+              if (child._active_) {
+                ret = true;
+              }
+            }
+            return ret;
+          }
+
+          return item == menu; //(item.id && item.id == menu.id) || (item.url && item.url == menu.url) || (item.path && item.path == menu.path) || (item.title && item.title == menu.title) || (item.label && item.label == menu.label) || (item.name && item.name == menu.name);
+        }
+      }
+
+      Vue.provide("selectMenu", selectMenu);
       return {
+        list,
         menuStyle,
       }
     },
     template: `
       <ul class="nav nav-pills nav-sidebar flex-column" :class="menuStyle.styles" data-widget="treeview" role="menu" data-accordion="false" style="overflow-x:hidden;">
-        <lte-tree-menu-item v-for="menu of menus" :menu="menu"></lte-tree-menu-item>
+        <lte-tree-menu-item v-for="menu of list" :menu="menu"></lte-tree-menu-item>
       </ul>
     `
 }
---
+++ js/animations.js
@@ -0,0 +1,98 @@
+
+// TODO: 需要验证在其它样式干扰下是否可以正常工作
+function slideDown(el, option) {
+	option = option || {};
+	// 必须设置overflow="hidden"才可以读到正确的offsetHeight
+	el.style.overflow = "hidden";
+	// 执行动画
+	animation({
+		speed: option.speed || 300,
+		distance: el.offsetHeight,
+		before: function() {
+			option.before && option.before();
+		},
+		action: function(value) {
+			//console.log("expand action", value);
+			el.style.height = value + "px";
+		},
+		after: function() {
+			option.after && option.after();
+		}
+	})
+}
+
+function slideUp(el, option) {
+	option = option || {};
+	// 设置overflow = "hidden"，变更height时内容才可以被遮盖，
+	// 否则高度虽然变了，内容依然会显示（与其它元素产生重合）
+	el.style.overflow = "hidden";
+	const height = el.offsetHeight;
+	animation({
+		speed: option.speed || 300,
+		distance: height,
+		before: function() {
+			option.before && option.before();
+		},
+		action: function(value) {
+			el.style.height = (height - value) + "px";
+		},
+		after: function() {
+			option.after && option.after();
+		}
+	})
+}
+
+function animation(option) {
+	var speed = option.speed || 0; // 动画时长
+	var distance = option.distance || 0; // 运动距离
+	var before = option.before || empty; // 动画开始前
+	var action = option.action || empty; // 动画执行中
+	var after = option.after || empty; // 动画结束后
+	//console.log("animation", distance)
+	// 动画执行前执行
+	before();
+	
+    // speed: 毫秒, 60:每秒执行频率
+	 // 步长 = 运动距离 / (执行总时长 / 1000毫秒 * 每秒执行频率)
+	var step = distance / (speed / 1000 * 60);
+	var start;
+
+	//console.log("step", step);
+	if (step == 0) return;
+
+	var w = window || {};
+	if (!w.requestAnimationFrame) {
+		w.requestAnimationFrame = w.webkitRequestAnimationFrame ||
+			w.mozRequestAnimationFrame ||
+			w.msRequestAnimationFrame ||
+			w.oRequestAnimationFrame ||
+			(function(cb) {
+				setTimeout(cb, 16)
+			});
+	}
+
+	function empty() {}
+
+	function doAction(timestamp) {
+		if (start === undefined) {
+			start = 0;
+		}
+		// 执行动画
+		action(Math.min(start, distance));
+
+		// 判断是否停止动画
+		if (start <= distance) {
+			//console.log("animation executing", start)
+			w.requestAnimationFrame(doAction);
+		} else {
+			//console.log("animation execute after", start)
+			// 动画结束后执行
+			after();
+		}
+
+		// 增加步长
+		start += step;
+	}
+
+	w.requestAnimationFrame(doAction);
+}
